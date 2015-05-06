5a86886e79b5581dbfd20700a77809f3c8b9e5f7
FelixB <felixbig@gmail.com>
2015-05-06 20:44:34 +0100

    generate 2 dipole freq plots

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ small_gamma = 1;
 alpha = 1/32;
 
 no_dipoles = 2;
-exchange_coupling = 0.3;
+exchange_coupling = 0.1;
 endcoupling = true;
 
 % create a system with these dipoles in a circle (equi) or line (lin)
@@ -15,20 +15,20 @@ G_x = calc_nn_gx(no_dipoles,exchange_coupling,endcoupling);
 
 
 %---------calculate the EXTERNAL_FIELD from 2 sources------------ 
-%{
+
 no_src = class_source([128;128],[0;0]);
 sources = [class_source([2;2], [1;-i]), no_src];  %location, field
 h_sources = calc_ss_h(calc_he(dipoles,sources,0));
-%}
+
 
 %---------run scripts to look at stuff------------
-L = calc_L(1.2,1,G_x,dipoles)
-hold on
+%L = calc_L(1.2,1,G_x,dipoles)
+
 fplot(@(w) calc_power(w,G_x,dipoles,h_sources,1), [0,2]);  %freq response
 
 %figure; fplot(@(he0) calc_power(0.9,G_x,dipoles,h_sources,he0), [0,2]);  %FMR (vary w gives interesting results)
 
-% figure; plot_eigenvalues(L, G_x, dipoles, [0,2], [4,6])  % plot the eigenvalues of response matrix
+ %figure; plot_eigenvalues(L, G_x, dipoles, [0,2], [3,4])  % plot the eigenvalues of response matrix
 
 %eai  %Reconstruct the L matrix
 
